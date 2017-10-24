50b78bf357d368d31e383b1982c8f17a02ef12ae
ramdan0cool <32842840+ramdan0cool@users.noreply.github.com>
2017-10-25 03:27:06 +0700

    Soal 1 2nd Rev
    Added Shared Memory
    Added command to view stock

--- Soal1.c
+++ Soal1.c
@@ -2,27 +2,55 @@
 #include<stdio.h> 
 #include<string.h> 
 #include<pthread.h> 
-//#include<stdlib.h> 
-#include<unistd.h> 
-//#include<stdlib.h>
+#include<stdlib.h> 
+#include<unistd.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdbool.h>
 
-pthread_t tid;
+int *total;
+const char *weapon[6] = {"MP4A1", "PM2-V1", "SPR-3", "SS2-V5", "SPG1-V3"};
+//key_t key = 1111;
+
+void *menu(){
+	bool exit = false;
+	char stock[7];
+	int senjata[4];
+	int command, amount;
+	//int total[7];
+        //const char *weapon[7] = {"MP4A1", "PM2-V1", "SPR-3", "SS2-V5", "SPG1-V3", "MINE"};
+	while(!exit){
+		printf ("Menu:\n1. Lihat Stock\n2. Tambah Stock\n");
+		scanf("%d", &command);
+		switch(command){
+		case 1:
+		printf("Stock senjata:\n");
+		for(int i=0; i<6; i++){
+		printf("%s %d\n", weapon[i], total[i]);
+		}
+		break;
+		case 2:
+		printf("Tambah senjata:\n");
+		scanf("%s %d", &stock, &amount);
+		printf("Nota: %s %d\n", stock, amount);
+		break;
+		case 3:
+		exit = true;
+		break;
+		default:
+		break;
+		}
+	}
+}
 
 int main(){
-	int senjata[4];
-	int command;
-	printf ("Menu:\n1. Lihat Stock\n2. Tambah Stock");
-	scanf("%d", &command);
-	switch(command){
-	case 1:
-	printf("Stock senjata:");
-	break;
-	case 2:
-	printf("Tambah senjata:");
-	break;
-	default:
-	break;
-	}
+	key_t key = 123;
+	//int total[7];
+	//const char *weapon[7] = {"MP4A1", "PM2-V1", "SPR-3", "SS2-V5", "SPG1-V3", "MINE"};
+	int shmid = shmget(key, sizeof(int)*3, IPC_CREAT | 0666);
+	total = shmat(shmid, NULL, 0);
+	menu();
 
 return 0;
 }
