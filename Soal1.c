fa383850368c90dc39e16eb15521c4c8bac1ce6f
ramdan0cool <32842840+ramdan0cool@users.noreply.github.com>
2017-10-25 03:35:50 +0700

    Soal 1 2nd Rev 2.0
    Added modify stock function

--- Soal1.c
+++ Soal1.c
@@ -10,9 +10,17 @@
 #include <stdbool.h>
 
 int *total;
-const char *weapon[6] = {"MP4A1", "PM2-V1", "SPR-3", "SS2-V5", "SPG1-V3"};
+const char *weapon[6] = {"MP4A1", "PM2-V1", "SPR-3", "SS2-V5", "SPG1-V3", "MINE"};
 //key_t key = 1111;
 
+void modifyStock(char weap[7], int amount){
+	if(strcmp(weap, "MP4A1") == 0) total[0] += amount;
+	else if(strcmp(weap, "PM2-V1") ==0) total[1] += amount;
+        else if(strcmp(weap, "SPR-3") ==0) total[2] += amount;
+        else if(strcmp(weap, "SS2-V5") ==0) total[3] += amount;
+        else if(strcmp(weap, "SPG1-V3") ==0) total[4] += amount;
+        else if(strcmp(weap, "MINE") ==0) total[5] += amount;
+}
 void *menu(){
 	bool exit = false;
 	char stock[7];
@@ -21,7 +29,7 @@ void *menu(){
 	//int total[7];
         //const char *weapon[7] = {"MP4A1", "PM2-V1", "SPR-3", "SS2-V5", "SPG1-V3", "MINE"};
 	while(!exit){
-		printf ("Menu:\n1. Lihat Stock\n2. Tambah Stock\n");
+		printf ("Menu:\n1. Lihat Stock\n2. Tambah Stock\n3. Exit\n");
 		scanf("%d", &command);
 		switch(command){
 		case 1:
@@ -33,6 +41,7 @@ void *menu(){
 		case 2:
 		printf("Tambah senjata:\n");
 		scanf("%s %d", &stock, &amount);
+		modifyStock(stock, amount);
 		printf("Nota: %s %d\n", stock, amount);
 		break;
 		case 3:
