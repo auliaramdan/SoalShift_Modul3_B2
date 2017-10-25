ac7156a4b1c78c71252b8034ed7febf5c79085ad
Unknown <muhajir.dzaki@gmail.com>
2017-10-25 11:16:45 +0700

    Edit soal5.c

--- soal5.c
+++ soal5.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-#include <thread.h>
+#include <pthread.h>
 #include <stdlib.h>
+#include <string.h>
+
+FILE *file;
 
 void *search(void *arg) {
 	int res = 0;
@@ -9,14 +12,25 @@ void *search(void *arg) {
 
 	while (fscanf(file,"%s", word) != EOF) {
 		if (strcmp(word, kata) == 0) res++;
-		printf("%s\n", word);
 	}
 
 	printf("%s : %d\n", kata, res);
 }
 
 int main(int argc, char *argv[]) {
+	file = fopen("Novel.txt", "r");
+	pthread_t tid[argc-1];
+	int i;
 
+	for (i = 0; i < argc-1; i++) {
+		pthread_create(&(tid[i]), NULL, &search, (void*) argv[i+1]);
+	}
+
+	for (i = 0; i < argc-1; i++) {
+		pthread_join(tid[i], NULL);
+	}
+
+	fclose(file);
 
 	return 0;
 }
