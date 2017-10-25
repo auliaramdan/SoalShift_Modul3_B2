8089aeda38e4c840210e323cf71c17b9f9662c16
Unknown <muhajir.dzaki@gmail.com>
2017-10-25 10:55:48 +0700

    Edit soal4.c

--- soal4.c
+++ soal4.c
@@ -14,7 +14,19 @@ void *faktorial(void *arg) {
 	printf("Hasil %d! = %.0Lf\n", num, res);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+	pthread_t tid[argc-1];
+	int i;
+
+	for (i = 0; i < argc-1; i++) {
+		int *nmr = (int*) malloc(sizeof(*nmr));
+		*nmr = atoi(argv[i+1]);
+		pthread_create(&(tid[i]), NULL, &faktorial, (void*) nmr);
+	}
+
+	for (i = 0; i < argc-1; i++) {
+		pthread_join(tid[i], NULL);
+	}
 
 	return 0;
 }
