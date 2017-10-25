56c60dacfdf6e8c0a4af41545dddcc47c44ca3fb
Unknown <muhajir.dzaki@gmail.com>
2017-10-25 10:46:32 +0700

    Edit soal4.c

--- soal4.c
+++ soal4.c
@@ -2,6 +2,18 @@
 #include <pthread.h>
 #include <stdlib.h>
 
+void *faktorial(void *arg) {
+	int num = (int *) arg;
+
+	int i;
+	long double res = 1;
+	for (i = 2; i <= num; i++) {
+		res *= i;
+	}
+
+	printf("Hasil %d! = %.0Lf\n", num, res);
+}
+
 int main() {
 
 	return 0;
