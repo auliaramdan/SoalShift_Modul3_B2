7d1eb515d9714f73071545494f852df4f9abe5e7
Unknown <muhajir.dzaki@gmail.com>
2017-10-25 13:39:29 +0700

    Edit soal2.c

--- soal2.c
+++ soal2.c
@@ -1,9 +1,23 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <string.h>
+
+char player[2][100];
+int lubang[2][16];
+
+void isilubang(int index_lubang, int player) {
+	lubang[player][index_lubang] = 1;
+}
 
 int main() {
-
+	system("clear");
+	int i;
+	for (i = 0; i < 2; i++) {
+		printf("Masukkan nama player %d: ", i+1);
+		scanf("%s", player[i]);
+		memset(lubang[i], 0, sizeof(int));
+	}
 
 	return 0;
 }
