d248bea003349b104c525a9ae266bd8c603f345e
Unknown <muhajir.dzaki@gmail.com>
2017-10-25 11:11:03 +0700

    Edit soal5.c

--- soal5.c
+++ soal5.c
@@ -2,6 +2,19 @@
 #include <thread.h>
 #include <stdlib.h>
 
+void *search(void *arg) {
+	int res = 0;
+	char *kata = (char *) arg;
+	char word[1024];
+
+	while (fscanf(file,"%s", word) != EOF) {
+		if (strcmp(word, kata) == 0) res++;
+		printf("%s\n", word);
+	}
+
+	printf("%s : %d\n", kata, res);
+}
+
 int main(int argc, char *argv[]) {
 
 
